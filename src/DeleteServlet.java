e180e8c74f8d8002958cba838dfef8a86f70703a
Mostafa-Farahat <mostafa.farahat78@gmail.com>
2024-09-17 13:44:33 +0300

    created template to store common js functions and implemented delete and downdload

---
+++ src/DeleteServlet.java
@@ -0,0 +1,73 @@
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.nio.file.*;
+import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet("/delete")
+public class DeleteServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //recursivly traverse the directory DFS and delete the folder
+        //when all files in it are deleted
+
+        String pathString = req.getParameter("path");
+        Path root = Paths.get(pathString);//path of file to be deleted
+
+        Files.walkFileTree(root, new SimpleFileVisitor<Path>(){
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attr){
+                try{
+                    Files.delete(file);
+                }catch(IOException ex){
+                    System.out.println("could not delete file recursively");
+                    return FileVisitResult.TERMINATE;
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+                    throws IOException
+            {
+                if(exc == null){
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }else{
+                    throw exc;
+                }
+            }
+        });
+
+        //overWriting the path string to make it equal to the path of the directory
+        Pattern regex = Pattern.compile("/home/mostafa/Desktop/SmolData");
+        Matcher matcher = regex.matcher(pathString);
+        pathString = matcher.replaceFirst("");
+
+        String[] arr = pathString.split("/");
+        arr[0]="";
+        arr[arr.length-1]="";
+        StringBuilder pathBuffer = new StringBuilder();
+        for(int i=0; i<arr.length-1; i++){
+            //perventing extra / for aesthetic purposes
+            if(i == arr.length-2){
+                pathBuffer.append(arr[i]);
+            }else{
+                pathBuffer.append(arr[i]).append("/");
+            }
+        }
+
+        pathString = pathBuffer.toString();
+        resp.sendRedirect("http://localhost:8080/SmolNAS/data" + pathString);
+    }
+}
---
+++ src/DownloadServlet.java
@@ -0,0 +1,56 @@
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@WebServlet("/download")
+public class DownloadServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(Files.isDirectory(Paths.get(req.getParameter("path")))){
+            resp.setContentType("text/plain");
+            resp.setHeader("Content-Disposition", "inline");
+            PrintWriter writer = resp.getWriter();
+            //TO DO: allow directory download of directories as a zip
+            writer.write("downloading directories is currently unavailable");
+            return;
+        }
+
+        resp.setContentType("application/octet-stream");
+        String[] arr = req.getParameter("path").split("/");
+        String fileName = arr[arr.length -1];
+        resp.setHeader("Content-Disposition", "attachment; filename="+fileName);
+        arr = null;
+
+        ServletOutputStream outStream = resp.getOutputStream();
+        File file = new File(req.getParameter("path"));
+        FileInputStream inputFile = new FileInputStream(file);
+
+        byte[] buffer = new byte[4000];
+        int bytesRead = 0;
+        bytesRead = inputFile.read(buffer);
+        while(bytesRead != -1){
+            outStream.write(buffer,0,bytesRead); //in order to not copy extra garbage bytes from the buffer
+            bytesRead = inputFile.read(buffer);
+        }
+        inputFile.close();
+        outStream.flush();
+        outStream.close();
+
+    }
+}
